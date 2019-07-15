04ac722b9ea2a5154bf7be7c8bf9fde792e637ee
QiAnShanghai <51881576+QiAnShanghai@users.noreply.github.com>
2019-07-15 13:32:22 +0200

    Add files via upload

---
+++ machinelearning.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import Mat
+
+class Neural:
+	def __init__(self,layersizes):
+		self.layersizes = []
+		self.layers = []
+		for i in layersizes:
+			self.layersizes.append(i)
+		for i in range(len(self.layers-1))
+			a = self.layers(i+1)
+			b = self.layers(i)
+			A = []
+			for j in range(a):
+				aa = []
+				for k in range(b):
+					aa.append(0)
+				A.append(aa)
+			self.layers.appennd(Mat.Mat(A))
